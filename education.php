b72c15c955a2ea255cbce46d6239ff7bfa58aaf3
jogindra_kumar <joginder9868@outlook.com>
2020-04-14 08:35:56 +0530

    ssb-portfolio

---
+++ education.php
@@ -0,0 +1,58 @@
+<br>
+<div class="container" id="education">
+                 <div class="row">
+                     <div class="col-sm-4">
+                         
+                         <h4 class="text-center text-primary"><i class="fas fa-user-graduate"></i> Education</h4>
+                     </div>
+                     <div class="col-sm-8">
+                     <div class="accordion" id="accordionExample">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          BACHELOR DEGREE OF COMPUTER SCIENCE AND ENGG. <i class="fas fa-graduation-cap"></i>
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          DIPLOMA IN COMPUTER ENGG. <i class="fas fa-book-open"></i>
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          HIGH SCHOOL SECONDARY EDUCATION <i class="fas fa-book-open"></i>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+                         
+                     </div>
+                     
+                     
+                 </div>
+             </div>  
---
+++ index.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/5033a2fd8c.js" crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <title>portfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        body{
+            font-family: 'Libre Baskerville', serif;
+/*            font-size: 22px;*/
+        }
+    
+         .front-head{
+          background: url(img/head-bg.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 90vh;
+                /* background: url('img/head-bg.jpg') no-repeat;
+            background-size: cover;
+            height: 100vh; */
+                
+          
+            }
+            .bg-contact{
+              background: linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)) , url('img/head-bg.jpg') no-repeat;
+            background-size: cover;
+            }
+       
+        
+       
+        .heading-text{
+            margin-top: 100px;
+            
+        }
+        
+        .profile-img{
+            height: 170px;
+            width: 200px;
+        }
+        .about{
+            font-size: 30px;
+        }
+        .skill{
+            font-size: 30px;
+        }
+        .personal-icon{
+          color:white;
+          font-size:35px;
+          margin-left:5em;
+          margin-top:2em;
+          margin-bottom:2em;
+        }
+        .gym-freak{
+          background: #2193b0;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #6dd5ed, #2193b0);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #6dd5ed, #2193b0); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+        }
+        .contact-bg{
+        background-color:#f0f5f5;
+        }
+        .gym-link{
+        
+          margin:30px 40px;
+          padding-left:5em;
+        
+          padding-right:5em;
+          background-color:white;
+          color:black;
+          text-align:center;
+        
+        }
+        .social-link{
+          font-size:20px;
+          
+        }
+        .contact-details{
+          margin-top:5em;
+          text-align:center;
+          color:#e6e6e6;
+        }
+        .icon:hover {
+    -webkit-animation: pulse 1s;
+    animation: pulse  1s;
+    
+}
+.dumbbell:hover{
+  -webkit-animation: bounce 1s;
+    animation: bounce 1s;
+}
+.navbar-light .navbar-nav .nav-link:hover {
+    color: white;
+}
+.abt-txt{
+  font-size:20px;
+}
+    </style>
+</head>
+<body>
+  
+<!--    top background image -->
+      <div class="front-head">
+       <div class="container">
+           
+           <div class="col-sm-12">
+               <div class="row justify-content-center">
+                   <nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand text-white" href="#">Suraj Bisht</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">About</a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="#education">Education</a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+      </li>
+     
+    
+    </ul>
+    
+  </div>
+</nav>
+                   
+                   
+                   
+               </div>
+               
+               
+           </div>
+           
+       </div>
+       <br>
+      <div class="container">
+         <div class="row heading-text">
+             
+             <div class="col-sm-12">
+              
+              
+              <h1 class="text-center text-white display-3">Welcome</h1>
+              
+          </div>
+             
+         </div>
+          
+          
+      </div>
+        
+      </div>
+        <br>
+<!--      end top background    -->
+<!--            about section-->
+           <div class="container mt-3">
+               
+               <div class="row">
+                   <div class="col-sm-4">
+                       
+                       <div class="container">
+                           <img src="img/abt2.jpeg" class="img-fluid rounded-circle profile-img" alt="">
+                           
+                           
+                       </div>
+                   </div>
+                   <div class="col-sm-8">
+                       
+                      <div class="container">
+                         <h5 class="about text-primary" id="about">About</h5>
+                          <p class="abt-txt">Hello, i'm suraj singh bisht from Delhi . i hold computer engineering degree this year 
+                          as 10 lakh student hold every year in India ,have lots of dream . i have done many interview
+                          most of want this skill most of want this really confusing actually. i really want get a chance to 
+                          prove myself as a fresher <i class="far fa-smile text-primary"></i> . <br> 
+                          <span class="badge badge-info">Quick learner</span> 
+                          <span class="badge badge-info">fitness freak</span> 
+                          <span class="badge badge-info">self-motivated</span>
+                          <span class="badge badge-info">creative</span><br>
+
+                         <span class="social-link text-primary"> <i class="fab fa-facebook-f"></i> 
+                         <i class="fab fa-twitter"></i>
+                         <i class="fab fa-instagram"></i>
+                         
+                         
+                         </span>
+                          </p>
+                          
+                          <br>
+                          
+                          <a href="#contact" class="btn btn-outline-primary mr-2">Hire me</a> <a href="resume.pdf" class="btn btn-outline-primary" download>Download CV</a>
+                      </div>
+                       
+                   </div>
+                   
+                   
+               </div>
+               
+           </div>   
+                
+                  
+             <br> 
+<!--                     end about section      -->
+                      
+                    
+                          
+                            
+<!--                  skill section end             -->
+
+<!--                  edcation section            -->
+                    <br>            
+                  <?php include "education.php"; ?>  
+             <br>  
+
+
+<div class="gym-freak">
+<h5 class="text-center text-white display-4"><i class="fas fa-dumbbell dumbbell"></i></h5>
+<h2 class="text-center text-white">fitness-freak</h2>
+<p class="text-center text-white">Fitness Help Me Feel Better <i class="far fa-smile"></i>  </p><br>
+
+<div class="container">
+<div class="row">
+
+<div class="col-sm-4 card-body">
+<img src="img/abt1.jpeg" class="img-fluid img-thumbnail icon" alt="">
+
+</div>
+
+<div class="col-sm-4 card-body">
+<img src="img/abt3.jpeg" class="img-fluid img-thumbnail icon" alt="">
+
+</div>
+<div class="col-sm-4 card-body">
+<img src="img/abt1.jpeg" class="img-fluid img-thumbnail icon" alt="">
+
+</div>
+
+
+
+
+</div>
+<br>
+</div>
+<div class="container">
+<h5 class="text-center display-3"></h5>
+<div class="row">
+<div class="col-sm-6 icon">
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="vid/vid_1.mp4" allowfullscreen></iframe>
+</div>
+</div>
+<div class="col-sm-6 icon">
+
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="vid/vid_2.mp4" allowfullscreen></iframe>
+</div>
+
+</div>
+
+</div>
+<br>
+
+</div>
+
+
+
+</div>
+<div>
+<br>
+</div>
+
+             
+             <br>
+             
+    <h1 class="text-center text-primary"><i class="fas fa-briefcase dumbbell"></i>  Hire me </h1>
+            <br>
+           <div class="contact-bg" id="contact">
+           
+           <div class="container">
+          <br>
+          <div class="row justify-content-center">
+           <div class="col-sm-4 bg-contact">
+           <div class="contact-details">
+           <h5><i class="fas fa-map-marker-alt"></i> Address</h5>
+           <p>Delhi</p>
+           
+           <h5><i class="fas fa-envelope"></i> E-mail</h5>
+           <p>surajbisht@gmail.com</p>
+           <span class="social-link text-primary"> <i class="fab fa-facebook-f"></i> 
+                         <i class="fab fa-twitter"></i>
+                         <i class="fab fa-instagram"></i>
+                         
+                         
+                         </span>
+           
+           </div>
+           
+           
+           </div>
+           <div class="col-sm-4">
+           <h5 class="text-center ">Get in touch</h5>
+           <form action="">
+            <div class="form-group">
+            <label for="">name</label>
+            <input type="text" class="form-control">
+            
+            </div>
+            <div class="form-group">
+            <label for="">email</label>
+            <input type="email" class="form-control">
+            
+            </div>
+            <div class="form-group">
+            <label for="">message</label>
+            <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+            
+            </div>
+            <div class="form-group">
+            <a href="" class="btn btn-primary form-control">Send Message</a>
+            
+            </div>
+           
+            </form>
+           
+            
+           </div>
+           
+           
+           </div>
+          
+          
+           <br>
+          
+          </div>
+           
+           
+           
+           </div>
+           
+        
+             <br>
+           
+        <div class="footer">
+        
+        <div class="footer-body">
+        <p class="footer-text text-center">
+        copyright <i class="fa fa-copyright" aria-hidden="true"></i>2019 
+        
+        </p>
+        
+        
+        </div>
+        
+        
+        </div>
+             
+             
+             
+                          
+                                      
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>                                          
+</body>
+</html>
